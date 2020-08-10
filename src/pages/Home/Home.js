fd9960fb293a54f85fd0bae7ba844e5fce9d50d2
detectiveno2 <nhatthien185@gmail.com>
2020-08-10 15:23:16 +0700

    CSS for HomePage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Home.css';
 
 function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <div className="HomeWrapper">
+        <div className="HomeLeft">
+          <h1>ChApp</h1>
+          <p>For group, for everyone, and for more...</p>
+          <div className="DirectArea">
+            <div className="AlignPaddingOne">
+              <Link to="/auth/login">Login</Link>
+            </div>
+            <div
+              style={{
+                textAlign: 'center',
+                padding: '0.5rem',
+              }}
+            >
+              <span>Or</span>
+            </div>
+            <div className="AlignPaddingOne">
+              <Link to="/auth/register">Register</Link>
+            </div>
+          </div>
+        </div>
+        <div className="HomeRight">
+          <img alt="home" src="./conrerence-call.png" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
